de4e43cf76719b2418e0d56abfed3d3843646d6b
Yougnsoo.han <gglife333@gmail.com>
2017-11-10 19:46:13 +0900

    Change ScrollView paddingLeft value

--- index.js
+++ index.js
@@ -1,252 +1,109 @@
-"use strict";
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
 
-import React, { Component } from "react";
-import PropTypes from 'prop-types';
+import React, { Component } from 'react';
 import {
-    Dimensions,
-    I18nManager,
-    ScrollView,
-    StyleSheet,
-    Text,
-    TouchableWithoutFeedback,
-    View
-} from "react-native";
+  Platform,
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  ScrollView,
+  Dimensions
+} from 'react-native';
+import Carousel from "./CardCarousel"
+const {width, height} = Dimensions.get('window')
 
-import styles from "./styles";
+export default class App extends Component {
 
-const { width } = Dimensions.get("window");
-
-export default class Carousel extends Component {
-
-    displayName = "Carousel";
-
-    static propTypes = {
-        pageStyle: PropTypes.object,
-        pageWidth: PropTypes.number,
-        children: PropTypes.oneOfType([ PropTypes.arrayOf(PropTypes.node), PropTypes.node ]).isRequired,
-        initialPage: PropTypes.number,
-        containerStyle: PropTypes.object,
-        noItemsText: PropTypes.string,
-        onPageChange: PropTypes.func,
-        sneak: PropTypes.number,
-        transitionDelay: PropTypes.number,
-        currentPage: PropTypes.number,
-        swipeThreshold: PropTypes.number
-    };
-
-    static defaultProps = {
-        initialPage: 0,
-        pageStyle: null,
-        containerStyle: null,
-        pageWidth: width - 80,
-        sneak: 20,
-        noItemsText: "Sorry, there are currently \n no items available",
-        transitionDelay: 0,
-        currentPage: 0,
-        swipeThreshold: 0.5
-    };
-
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            gap: undefined,
-            currentPage: props.currentPage,
-        };
-
-        this._scrollTimeout = null;
-
-        this._resetScrollPosition = this._resetScrollPosition.bind(this);
-        this._handleScrollEnd = this._handleScrollEnd.bind(this);
+  constructor(props){
+    super(props)
+    this.state = {
+      movies: []
     }
+  }
 
-    componentWillMount() {
-        this._calculateGap(this.props);
-    }
+  componentDidMount() {
+    fetch('https://facebook.github.io/react-native/movies.json')
+    .then((response) => response.json())
+    .then((responseJson) => {
+      this.setState({movies:responseJson.movies})
+    })
+    .catch((error) => {
+      console.error(error);
+    }) 
+  }
 
-    componentDidMount() {
-        this._resetScrollPosition(false);
-    }
+  render() {
 
-    componentWillReceiveProps(nextProps) {
-        
-        this.setState({
-            currentPage: nextProps.currentPage
-        });
-        
-        this._calculateGap(nextProps);
-    }
-
-    componentDidUpdate(prevProps, prevState) {
-        if (prevProps.currentPage !== this.props.currentPage) {
-            this._resetScrollPosition();
-            this._onPageChange(this.props.currentPage);
-        } else if (prevState.currentPage !== this.state.currentPage) {
-            this._resetScrollPosition();
-            this._onPageChange(this.state.currentPage);
-        }
-    }
-
-    componentWillUnmount() {
-        if (this._scrollTimeout) {
-            clearTimeout(this._scrollTimeout);
-        }
-    }
-
-    _getPageOffset() {
-        const {
-            pageWidth,
-        } = this.props;
-
-        const {
-            gap,
-        } = this.state;
-
-        return pageWidth + gap;
-    }
-
-    _getPageScrollX(pageIndex) {
-        return pageIndex * this._getPageOffset();
-    }
-
-    _getPagesCount() {
-        return React.Children.count(this.props.children);
-    }
-
-    _resetScrollPosition(animated = true) {
-        // in android, you can't scroll directly in componentDidMount
-        // (http://stackoverflow.com/questions/33208477/react-native-android-scrollview-scrollto-not-working)
-        // however this doesn't work in android for some reason:
-        // InteractionManager.runAfterInteractions(() => {
-        //     this.scrollView.scrollTo({ y: 0, x: pagePosition}, true);
-        //     console.log('scrollView.scrollTo x:', pagePosition);
-        // });
-        // So I was left with an arbitrary timeout.
-        if (this._scrollTimeout) {
-            clearTimeout(this._scrollTimeout);
-        }
-        this._scrollTimeout = setTimeout(() => {
-            this.scrollView.scrollTo({
-                x: this._getPageScrollX(this.state.currentPage),
-                y: 0,
-                animated,
-            });
-            this._scrollTimeout = null;
-        }, this.props.transitionDelay);
-    }
-
-    _calculateGap(props) {
-        const { sneak, pageWidth } = props;
-        if (pageWidth > width) {
-            throw new Error("invalid pageWidth");
-        }
-        /*
-         ------------
-        |            |
-        |-   ----   -|
-        | | |    | | |
-        | | |    | | |
-        | | |    | | |
-        |-   ----   -|
-        |^-- sneak   |
-        |         ^--- gap
-         ------------
-
-        */
-        const gap = (width - (2 * sneak) - pageWidth) / 2;
-        this.setState({gap: gap});
-    }
-
-    _handleScrollEnd(e) {
-        const { swipeThreshold } = this.props;
-        const { currentPage } = this.state;
-
-        const currentPageScrollX = this._getPageScrollX(currentPage);
-        const currentScrollX = e.nativeEvent.contentOffset.x;
-
-        const swiped = (currentScrollX - currentPageScrollX) / this._getPageOffset();
-
-        const pagesSwiped = Math.floor(Math.abs(swiped) + (1 - swipeThreshold)) * Math.sign(swiped);
-        const newPage = Math.max(Math.min(currentPage + pagesSwiped, this._getPagesCount() - 1), 0)
-
-        if (newPage !== currentPage) {
-            this.setState({currentPage: newPage});
-        } else {
-            this._resetScrollPosition();
-        }
-
-    };
-
-    _onPageChange(position) {
-        if (this.props.onPageChange) {
-            const currentElement = this.props.children[position];
-            this.props.onPageChange(position, currentElement);
-        }
-    }
-
-    render() {
-        const { sneak, pageWidth } = this.props;
-        const { gap } = this.state;
-        const computedStyles = StyleSheet.create({
-            scrollView: {
-                paddingLeft: sneak + gap / 2,
-                paddingRight: sneak + gap / 2
-            },
-            page: {
-                width: pageWidth,
-                justifyContent: "center",
-                marginLeft: gap / 2,
-                marginRight: gap / 2
-            }
-        });
-
-        // if no children render a no items dummy page without callbacks
-        let body = null;
-        if (!this.props.children) {
-            body = (
-                <TouchableWithoutFeedback>
-                    <View style={ [ styles.page, computedStyles.page, this.props.pageStyle ] }>
-                        <Text style={ styles.noItemsText }>
-                            { this.props.noItemsText }
-                        </Text>
-                    </View>
-                </TouchableWithoutFeedback>
-            );
-        }
-        else {
-            const children = Array.isArray(this.props.children) ? this.props.children : [this.props.children]
-            body = children.map((c, index) => {
-                return (
-                    <TouchableWithoutFeedback
-                        key={ index }
-                        onPress={ () => this.setState({currentPage: index}) }
-                    >
-                        <View
-                            style={ [ styles.page, computedStyles.page, this.props.pageStyle ] }
-                        >
-                            { c }
-                        </View>
-                    </TouchableWithoutFeedback>
-                );
-            });
-        }
-
-        return (
-            <View style={[ styles.container, this.props.containerStyle ]}>
-                <ScrollView
-                    automaticallyAdjustContentInsets={ false }
-                    bounces
-                    contentContainerStyle={ [ computedStyles.scrollView ] }
-                    style={{ flexDirection: (I18nManager && I18nManager.isRTL) ? 'row-reverse' : 'row' }}
-                    decelerationRate={ 0.9 }
-                    horizontal
-                    onScrollEndDrag={ this._handleScrollEnd }
-                    ref={ c => this.scrollView = c }
-                    showsHorizontalScrollIndicator={ false }
-                >
-                    { body }
-                </ScrollView>
+    console.log("reRender!")
+  
+    return (
+      <View style = {styles.container}>
+        <View style = {{flex: 1, backgroundColor: 'red'}}/>
+        <View style = {{flex: 3.5, backgroundColor: 'skyblue'}}>
+        <FlatList 
+          onMomentumScrollBegin = {()=> console.log("onMomentumScrollBegin")}
+          onMomentumScrollEnd = {()=>console.log("onMomentumScrollEnd")}
+          onScroll = {()=>console.log("onScroll")}
+          stickyHeaderIndices={[0]}
+          data={this.state.movies}
+          contentContainerStyle = {{paddingTop: 100}}
+          keyExtractor={(_,index) => index}
+          renderItem={({item}) => {
+            return(
+            <View style ={{justifyContent: 'center', alignItems: 'center', padding: 20}}>
+              <Text>{item.title}</Text>
             </View>
-        );
-    }
+            )
+          }
+        }
+        />
+        </View>
+        <View style = {{flex:2, backgroundColor: 'rgb(216, 224, 98)', alignItems: 'center'}}>
+        <Carousel containerStyle = {{ backgroundColor: 'blue',}}  pageStyle={ {backgroundColor: "white", borderRadius: 10, height: 180} } sneak = {60}
+                  pageWidth = {200}
+        >
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+          <Text>Hello</Text>
+        </Carousel>
+        </View>
+      </View>
+    )
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  card: {
+    width : width * 0.9,
+    justifyContent: 'center',
+    backgroundColor: 'white',
+    height: 100,
+    marginHorizontal: 10
+
+  },
+  cardContents: {
+    alignSelf: 'center',
+    textAlign: 'center',
+    fontSize: 20,
+    fontWeight: '500'
+  }
+})
+
+const Card = props => (
+  <View style = {styles.card}>
+    <Text style = {styles.cardContents}>{props.number}</Text>
+  </View>
+)
